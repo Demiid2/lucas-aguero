e22c179069e322c53373b6b1b48d9513c1b696a8
Demian Oscar Gonzalez <demiandev2@gmail.com>
2023-08-06 14:53:11 -0300

    changes

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -1,10 +1,11 @@
-import lucasaguero from '../images/Lucas.jpeg'
+import lucasaguero from '../images/lucas2.jpeg'
 function Inicio() {
     return(
         <>
         <header className='header'>
         <h1>En Moreno, es Lucas Agüero</h1>
         <img className='image' src={lucasaguero} alt="lucas aguero"/>
+        <br /> <br />
         </header>
         </>
     )
--- src/components/Somos.jsx
+++ src/components/Somos.jsx
@@ -1,11 +1,19 @@
 import { Image } from 'react-bootstrap';
 import imag from '../images/precan1.jpeg'
 import imag1 from '../images/precan2.jpeg'
+import imag2 from '../images/precan3.jpeg'
+import imag3 from '../images/precan4.jpeg'
+import imag4 from '../images/precan5.jpeg'
+import imag5 from '../images/precan6.jpeg'
 function Somos() {
     return(
         <div className='text-center'>
         <Image src={imag} className="mx-auto p-5 img-fluid" />
         <Image src={imag1} className="mx-auto p-5 img-fluid" />
+        <Image src={imag2} className="mx-auto p-5 img-fluid" />
+        <Image src={imag3} className="mx-auto p-5 img-fluid" />
+        <Image src={imag4} className="mx-auto p-5 img-fluid" />
+        <Image src={imag5} className="mx-auto p-5 img-fluid" />
         </div>
     )
 }
